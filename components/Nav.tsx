8b8ca2a028b6aa825e398762558edc494f36b859
h3zord <lucaschmain@gmail.com>
2023-09-27 15:58:15 -0300

    feat: adding navigation bar structure

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,23 @@
 "use client"
 
-import { signOut } from "next-auth/react";
+import { getProviders, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Nav() {
   const isUserLoggedIn = true
+
+  const [providers, setProviders] = useState(null)
+  const [toggleDropDown, setToggleDropDown] = useState(false)
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders()
+
+      setProviders(response)
+    }
+  }, [])
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -31,12 +43,96 @@ export default function Nav() {
             </button>
 
             <Link href="/profile">
-
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="Profile"
+              />
             </Link>
           </div>
         ) : (
-          <></>
+          <>
+            {
+              providers && Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))
+            }
+          </>
         )
+        }
+      </div>
+
+      <div className="sm:hidden flex relative">
+        {
+          isUserLoggedIn ? (
+            <div className="flex ">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="Profile"
+                onClick={() => setToggleDropDown((prevState) => !prevState)}
+              />
+
+              {
+                toggleDropDown && (
+                  <div className="dropdown">
+                    <Link
+                      href="/profile"
+                      className="dropdown_link"
+                      onClick={() => setToggleDropDown(false)}
+                    >
+                      My Profile
+                    </Link>
+
+                    <Link
+                      href="/create-prompt"
+                      className="dropdown_link"
+                      onClick={() => setToggleDropDown(false)}
+                    >
+                      Create Prompt
+                    </Link>
+
+                    <button
+                      type="button"
+                      className="mt-5 w-full black_btn"
+                      onClick={() => {
+                        setToggleDropDown(false)
+                        signOut()
+                      }}
+                    >
+                      Sign Out
+                    </button>
+                  </div>
+                )
+              }
+            </div>
+          ) : (
+            <>
+              {
+                providers && Object.values(providers).map((provider) => (
+                  <button
+                    type="button"
+                    key={provider.name}
+                    onClick={() => signIn(provider.id)}
+                    className="black_btn"
+                  >
+                    Sign In
+                  </button>
+                ))
+              }
+            </>
+          )
         }
       </div>
     </nav>
